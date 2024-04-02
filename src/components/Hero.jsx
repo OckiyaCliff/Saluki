cf7022a1ac1c0d840aa8086226cfc6b8d2e22ff5
Cliff <Ockiyacliffc@gmail.com>
2024-04-02 14:41:48 +0100

    hero update 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,7 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
-import Generating from "./Generating";
-import Notification from "./Notification";
-import CompanyLogos from "./CompanyLogos";
+
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -57,9 +55,6 @@ const Hero = () => {
                   height={690}
                   alt="rolex"
                 />
-
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
-
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
@@ -68,13 +63,6 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
-                </ScrollParallax>
-
-                <ScrollParallax isAbsolutelyPositioned>
-                  <Notification
-                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
-                    title="Code generation"
-                  />
                 </ScrollParallax>
               </div>
             </div>
@@ -94,7 +82,6 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
 
       <BottomLine />
